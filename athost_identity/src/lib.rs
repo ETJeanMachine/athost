fe4d1402013ac6ba24c465bd8afd8454b3011010
Eric Hamilton <ericjean873@gmail.com>
2025-04-26 14:39:33 -0700

    Improved validation for the from_string function in the identity crate.

--- athost_identity/src/lib.rs
+++ athost_identity/src/lib.rs
@@ -1,51 +1,80 @@
-pub enum DIDType {
+use regex::Regex;
+
+pub enum DIDMethod {
     Web,
     PLC,
 }
 
-impl DIDType {
+impl DIDMethod {
+    fn from_str(s: &str) -> Result<Self, String> {
+        match s {
+            "web" => Ok(DIDMethod::Web),
+            "plc" => Ok(DIDMethod::PLC),
+            _ => Err("Unsupported DID method".to_string()),
+        }
+    }
+
     fn as_str(&self) -> &'static str {
         match self {
-            DIDType::Web => "web",
-            DIDType::PLC => "plc",
+            DIDMethod::Web => "web",
+            DIDMethod::PLC => "plc",
         }
     }
 }
 
 pub struct DID {
-    did_type: DIDType,
-    value: String,
+    method: DIDMethod,
+    identifier: String,
 }
 
 impl ToString for DID {
     fn to_string(&self) -> String {
-        format!("did:{}:{}", self.did_type.as_str(), self.value)
+        format!("did:{}:{}", self.method.as_str(), self.identifier)
     }
 }
 
 impl DID {
-    pub fn new(did_type: DIDType, value: String) -> Self {
-        DID { did_type, value }
+    pub fn new(did_type: DIDMethod, value: String) -> Self {
+        DID {
+            method: did_type,
+            identifier: value,
+        }
     }
 
+    /// Parses a DID string into a DID struct, and validates the format.
+    ///
+    /// # Arguments
+    ///
+    /// `did` - A string slice representing a DID in the format "did:method:identifier"
+    ///
+    /// # Returns
+    ///
+    /// `Result<Self, String>` - A Result containing either the parsed DID or an error message.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let did = DID::from_string("did:web:example.com").unwrap();
+    /// ```
     pub fn from_string(did: &str) -> Result<Self, String> {
-        let parts: Vec<&str> = did.split(':').collect();
-        if parts.len() != 3 {
-            return Err("Invalid DID format".to_string());
+        let re = Regex::new(r"^did:[a-z]+:[a-zA-Z0-9._:%-]*[a-zA-Z0-9._-]$").unwrap();
+        if !re.is_match(did) {
+            return Err("Invalid DID Syntax.".to_string());
         }
-        let did_type = match parts[1] {
-            "web" => DIDType::Web,
-            "plc" => DIDType::PLC,
-            _ => return Err("Unsupported DID type".to_string()),
-        };
+        let parts: Vec<&str> = did.split(':').collect();
+        // will return an error here if the method isn't supported
+        let method = DIDMethod::from_str(parts[1])?;
         Ok(DID {
-            did_type,
-            value: parts[2].to_string(),
+            method,
+            identifier: parts[2].to_string(),
         })
     }
 
-    pub fn resolve_handle() {
-        todo!()
+    pub async fn resolve_handle(&self) -> Result<(), String> {
+        match self.method {
+            DIDMethod::Web => todo!(),
+            DIDMethod::PLC => todo!(),
+        }
     }
 
     pub fn uri(&self) -> String {
