c6faf44aae035fa3827946b13294217d2f71fe05
Eric Hamilton <ericjean873@gmail.com>
2025-05-03 10:06:16 -0700

    some random tests in the main.rs file lol

--- athost/src/main.rs
+++ athost/src/main.rs
@@ -2,12 +2,13 @@ use athost_identity::DID; // imports the DID struct
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let did_test: DID = DID::from_str("did:web:nat.vg")?;
+    let did_web_test: DID = DID::from_str("did:web:nat.vg")?;
     let did_plc_test: DID = DID::from_str("did:plc:6vxtya3serxcwvcdk5e7psvv")?;
-    println!("{}", did_test.to_string());
-    let document = did_test.fetch_document().await?;
-    println!("{:?}", document);
-    let document_plc = did_plc_test.fetch_document().await?;
-    println!("{:?}", document_plc);
+    println!("{}", did_web_test.to_string());
+    println!("{}", did_plc_test.to_string());
+    let handle_web = did_web_test.resolve_handle().await?;
+    let handle_plc = did_plc_test.resolve_handle().await?;
+    println!("{}", handle_web);
+    println!("{}", handle_plc);
     Ok(())
 }
